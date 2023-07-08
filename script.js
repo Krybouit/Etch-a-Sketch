5545375533747ea23bb74286344daaccee901df7
Cristian Aton <cristianeduardo0414@hotmail.com>
2023-07-08 05:58:44 -0400

    User input grid size
    changes the grid size between 2-100 according to the user input

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const gridContainer = document.getElementById("grid-container");
+const button = document.querySelector("button");
+
+button.addEventListener("click", gridSize);
 
 
-function box() {
-    for (i = 0; i < 256; i++) {
+function gridSize() {
+let sizeNumber = prompt("Choose grid size from (2-100)");
+if (sizeNumber > 100 || sizeNumber == 0 ||  sizeNumber == 1 || isNaN(sizeNumber)) {
+    alert("Please choose a number between (2-100)");
+    return gridSize();
+}
+gridContainer.style.gridTemplateColumns = `repeat(${sizeNumber}, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${sizeNumber}, 1fr)`;
+box(sizeNumber);
+}
+
+function resetGrid(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function box(size) {
+    resetGrid(gridContainer);
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.setAttribute("class","box");
         gridContainer.appendChild(square);
         square.addEventListener("mouseover", function(event) {
             event.target.style.backgroundColor = "black";
         })    
+        
     }
 }
 
-box();
+box(16);
 
   
 
