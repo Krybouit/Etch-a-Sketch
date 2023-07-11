e119a922906030477e14ea30ec208f35f194c6c2
Cristian Aton <cristianeduardo0414@hotmail.com>
2023-07-11 06:19:54 -0400

    Optimize performance
    * removes event listeners from for loop, making it less laggy
    when changing grid sizes
    
    *increases max grid size to 100x100

--- script.js
+++ script.js
@@ -2,15 +2,8 @@ const gridContainer = document.getElementById("grid-container");
 const clearBtn = document.getElementById("clearBtn");
 const sliderValue = document.getElementById("sliderValue");
 const valueOutput = document.getElementById("valueOutput");
-valueOutput.innerHTML = sliderValue.value;
+valueOutput.innerHTML = "16x16";
 
-sliderValue.oninput = function gridSize() {
-    let sizeNumber = sliderValue.value;
-    gridContainer.style.gridTemplateColumns = `repeat(${sizeNumber}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${sizeNumber}, 1fr)`;
-    valueOutput.innerHTML = sliderValue.value;
-    box(sizeNumber);
-}
 
 
 function box(size) {
@@ -19,16 +12,31 @@ function box(size) {
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (i = 0; i < size * size; i++) {
         const square = document.createElement("div");
-        square.setAttribute("class","box");
         gridContainer.appendChild(square);
-        square.addEventListener("mouseover", function(event) {
-            event.target.style.backgroundColor = "black";
-        })
-        clearBtn.addEventListener("click", function() {
-            square.style.backgroundColor = "white";
-        })
+        square.setAttribute("class","box");
     }
 }
+
+sliderValue.oninput = function gridSize() {
+    let sizeNumber = sliderValue.value;
+    gridContainer.style.gridTemplateColumns = `repeat(${sizeNumber}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${sizeNumber}, 1fr)`;
+    valueOutput.innerHTML = sliderValue.value + "x" + sliderValue.value;
+    box(sizeNumber);
+}
+
+
+gridContainer.addEventListener("mouseover", function(event) {
+    event.target.style.backgroundColor = "black";
+});
+
+
+clearBtn.addEventListener("click", function() {
+    let erase = document.querySelectorAll(".box");
+    erase.forEach(element => {
+        element.style.backgroundColor = "white";
+    })
+});
 
 
 function resetGrid(parent) {
