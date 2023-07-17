4e2abdc30adf99c94207491094057acde1d17ff2
Cristian Aton <cristianeduardo0414@hotmail.com>
2023-07-17 04:42:33 -0400

    git Update button CSS
    adds a push effect for the buttons

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ const valueOutput = document.getElementById("valueOutput");
 const colorPicker = document.getElementById("colorPicker");
 const eraser = document.getElementById("eraser");
 const pen = document.getElementById("pen");
+const press = document.querySelectorAll(".press");
 let color = "#000000";
 valueOutput.innerHTML = "16x16";
-
 
 
 function box(size) {
     resetGrid(gridContainer);
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    draw();
     for (i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         gridContainer.appendChild(square);
@@ -26,29 +25,10 @@ sliderValue.oninput = function gridSize() {
     let sizeNumber = sliderValue.value;
     gridContainer.style.gridTemplateColumns = `repeat(${sizeNumber}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${sizeNumber}, 1fr)`;
-    valueOutput.innerHTML = sliderValue.value + "x" + sliderValue.value;
+    valueOutput.innerText = sliderValue.value + "x" + sliderValue.value;
     box(sizeNumber);
 }
 
-
-function selectColor(event) {
-   color = event.target.value;
-}
-
-
-
-function erase() {
-    gridContainer.addEventListener("mouseover", function(event) {
-        event.target.style.backgroundColor = "white";
-    });
-}
-
-
-function resetGrid(parent) {
-    while(parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-}
 
 function draw() {
     gridContainer.addEventListener("mouseover", function(event) {
@@ -56,20 +36,45 @@ function draw() {
     });
 }
 
-eraser.addEventListener("click", erase);
+function selectColor(event) {
+   color = event.target.value;
+   document.getElementById("eraser").classList.remove("focus");
+   document.getElementById("pen").classList.add("focus");
+}
 
-pen.addEventListener("click", draw);
+function erase() {
+    gridContainer.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "white";
+    });
+}
 
-colorPicker.addEventListener("change", (event) => selectColor(event));
-colorPicker.addEventListener("click", draw);
-
+function resetGrid(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 clearBtn.addEventListener("click", function() {
     const clear = document.querySelectorAll(".box");
     clear.forEach(element => {
         element.style.backgroundColor = "white";
     })
-});
+})
+
+press.forEach(element => {
+    element.addEventListener("click", function buttonFocus() {
+        document.querySelector(".focus").classList.remove("focus");
+        element.classList.add("focus");
+    })
+})
+
+pen.addEventListener("click", draw);
+
+colorPicker.addEventListener("change", (event) => selectColor(event));
+
+colorPicker.addEventListener("click", draw);
+
+eraser.addEventListener("click", erase);
 
 draw();
 box(16);
